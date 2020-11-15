138091a6945b5ee7ff43cd6be3ba5ce5ae556f91
denemcd <denemcdonald@gmail.com>
2020-11-16 11:51:09 +1300

    Upgraded script

--- test.py
+++ test.py
@@ -4,4 +4,8 @@ config_file = open("config.json")
 config_data = json.load(config_file)
 config_file.close()
 
-print(config_data)
+dene = {"Userid": config_data["UserID"], "Name": config_data["Username"],
+        "Email": config_data["Email"], "apiKey": config_data["apiKey"]}
+
+
+print(dene["Name"])
