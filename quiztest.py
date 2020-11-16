8e4ef77bb0fb99dd7b253e80611d12ebc0da7b1f
denemcd <denemcdonald@gmail.com>
2020-11-16 15:23:45 +1300

    Improved quiz engine

--- quiztest.py
+++ quiztest.py
@@ -19,15 +19,18 @@ for question in questions["results"]:  # Process the questions
     question_number += 1
 
     question_text = question["question"]
-    question_text = urllib.parse.unquote(question_text)
+    question_text = urllib.parse.unquote(
+        question_text)  # Convert text from URL encoding
     correct_answer = question["correct_answer"]
     correct_answer = urllib.parse.unquote(correct_answer)
     answers = [question["correct_answer"], question["incorrect_answers"][0],
                question["incorrect_answers"][1], question["incorrect_answers"][2]]
 
     for x in range(0, 4):
+        # Convert Questions from URL Encoding
         answers[x] = urllib.parse.unquote(answers[x])
 
+    # Ask the question
     print("\nQuestion " + str(question_number) + ": " + question_text)
 
     answers = random.sample(answers, k=4)  # randomise answers
@@ -38,7 +41,7 @@ for question in questions["results"]:  # Process the questions
         print(str(x) + ": " + answer)
 
     while valid_answer == False:  # Await the users answer
-        user_answer = int(input("Choose answer: "))
+        user_answer = int(input("\nChoose answer: "))
         if user_answer == 1 or user_answer == 2 or user_answer == 3 or user_answer == 4:
             valid_answer = True
 
@@ -59,3 +62,9 @@ for question in questions["results"]:  # Process the questions
 # End the game
 
 print("Your score was " + str(score))
+if score >= 8:
+    print("Excellent job!")
+elif score > 4 and score < 8:
+    print("Middle of the road")
+else:
+    print("Come on... that's not good")
